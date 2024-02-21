3bc7b3d39e5d33d996869217e1a614041e453100
Jorge Simondi <simondi.jorge@gmail.com>
2024-02-21 02:01:06 -0300

    chore: limpeza do app.component.ts para remover os testes do servico

--- Pokedex/src/app/app.component.ts
+++ Pokedex/src/app/app.component.ts
@@ -7,28 +7,4 @@ import { Pokemon } from './models/pokemon-models';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  title: string = 'Pokedex';
-  limit: number = 10;
-  offset: number = 0;
-  typeLimit: number = 160;
-  typeOffset: number = 146;
-  pokemonsToShow: Pokemon[] = [];
-
-  constructor(private pokemonService: PokeAPIService) {}
-
-  ngOnInit() {
-    this.pokemonService
-      .getPokemons(this.offset, this.limit)
-      .subscribe((res: Pokemon[]) => {
-        console.log('res:', res);
-        this.pokemonsToShow = res;
-      });
-
-    this.pokemonService
-      .getPokemonByType('grass', this.typeOffset, this.typeLimit)
-      .subscribe(res => {
-        console.log('tipo grama:', res);
-      });
-  }
-}
+export class AppComponent {}
